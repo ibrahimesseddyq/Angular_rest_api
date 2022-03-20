cdc1dc24428b0e27a95ab3f07b8e1f61a2d39621
armanovxx <iebeat2018@outlook.com>
2022-03-20 17:20:49 +0100

    update server.js

--- api_endpoint/server.js
+++ api_endpoint/server.js
@@ -1,9 +1,8 @@
 let express = require("express");
 const app = express();
-let fs = require('fs');
 const cors = require("cors");
 app.use(cors());
-const data = fs.readFile("./db.json");
+const data = {};
 const port = process.env.NODE_ENV ||
     8080;
 app.get('/data', (req, res) => {
